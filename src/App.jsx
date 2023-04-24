6f37478b5de9326d9203c6943069b8dc6ec6dbaf
carlos <carlos.chacon@gexnova.com.co>
2023-04-24 18:09:36 -0500

    47.46 min 24042023

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,33 @@
 import React from "react";
 import { useState } from "react";
+import confetti from "canvas-confetti"
+
+import { Square } from "./components/Square"
+import { checkWinner } from "./logic/board"
+import { TURNS } from "./constants"
+
 import "./App.css";
 
-const TURNS = {
-  X: "x",
-  O: "o",
-};
 
-const WINNER_COMBOS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-const Square = ({ children, isSelected, updateBoard, index }) => {
-  const className = `square ${isSelected ? "is-selected" : ""}`;
-  const handleClick = () => {
-    updateBoard(index);
-  };
-  return (
-    <div className={className} onClick={handleClick}>
-      {children}
-    </div>
-  );
-};
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
   const [winner, setWinner] = useState(null);
-  const checkWinner = (boardToCheck) => {
-    for (const combo of WINNER_COMBOS) {
-      const [a,b,c] = combo
-      if(
-        boardToCheck[a] &&
-        boardToCheck[a] === boardToCheck[b] &&
-        boardToCheck[a] === boardToCheck[c]
-      ) return boardToCheck[a]
-    }
-    return null
+
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const checkEndGame = (board) => {
+    // revisamos si hay un empate
+    //si no hay mas espacios vacíos en el tablero
+    return board.every((square) => square !== null)
   }
+
   const updateBoard = (index) => {
     // no se actualiza esta posición si ya tiene algo
     if (board[index] || winner) return;
@@ -57,42 +40,52 @@ function App() {
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
     //revisar si hay ganador
-    const newWinner = checkWinner(newBoard)
-    if(newWinner) setWinner(newWinner)
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      confetti()
+      setWinner(newWinner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false);
+    }
   };
 
   return (
     <main className="board">
       <h1>Triqui Triqui</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
-        {board.map((value, index) => {
+        {board.map((square, index) => {
           return (
             <Square key={index} updateBoard={updateBoard} index={index}>
-              {board[index]}
+              {square}
             </Square>
           );
         })}
       </section>
       <section className="turn">
-        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square
+          isSelected={turn === TURNS.X}
+          updateBoard={undefined}
+          index={undefined}
+        >
+          {TURNS.X}
+        </Square>
         <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
-      
-      {
-        winner !== null && (
-          <section className="winner">
-            <div className="text">
-              <h2>
-                {
-                  winner === false
-                  ? 'Empate'
-                  : 'Ganó ' + winner
-                }
-              </h2>
-            </div>
-          </section>
-        )
-      }
+
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Empate" : "Ganó "}</h2>
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </main>
   );
 }
