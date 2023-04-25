11ebb5c1ad2cdfae024162b461718e9d9d36d307
carlos <carlos.chacon@gexnova.com.co>
2023-04-25 16:29:18 -0500

    localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,18 @@ function App() {
     const boardFromStorage = window.localStorage.getItem("board");
     return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
   })
-  const [turn, setTurn] = useState(TURNS.X);
+  const [turn, setTurn] = useState(() => {
+    const turnFormStorage = window.localStorage.getItem("turn");
+    return  turnFormStorage ?? TURNS.X
+  });
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setTurn(TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
   };
 
   const updateBoard = (index) => {
@@ -36,7 +41,7 @@ function App() {
     setTurn(newTurn);
     // guardar aquí partida
     window.localStorage.setItem("board", JSON.stringify(newBoard));
-    window.localStorage.setItem("turn", turn);
+    window.localStorage.setItem("turn", newTurn);
     //revisar si hay ganador
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
